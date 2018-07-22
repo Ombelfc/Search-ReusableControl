f06dbab0b4271ce1e8eecb1dcbc05de362820ca6
Omar <berraj97@gmail.com>
2018-07-22 11:57:58 +0200

    Added baseCcommand and partial searchViewModel

---
+++ SearchByRegex/Commands/BaseCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SearchByRegex.Commands
+{
+    public class BaseCommand : ICommand
+    {
+        private Action methodToExecute;
+        private Func<bool> canExecuteMethod;
+
+        public BaseCommand(Action methodToExecute, Func<bool> canExecuteMethod)
+        {
+            this.methodToExecute = methodToExecute;
+            this.canExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        #region ICommand Interface
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecuteMethod != null)
+                return canExecuteMethod();
+
+            if (methodToExecute != null)
+                return true;
+
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            methodToExecute?.Invoke();
+        }
+    }
+}
---
+++ SearchByRegex/ViewModels/SearchViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using SearchByRegex.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace SearchByRegex.ViewModels
+{
+    public class SearchViewModel
+    {
+        public BaseCommand OpenCommand { get; set; }
+
+        public SearchViewModel()
+        {
+            OpenCommand = new BaseCommand(OnOpen, CanOpen);
+        }
+
+        private void OnOpen()
+        {
+            TextRange range;
+            FileStream fStream;
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.DefaultExt = ".txt";
+            ofd.Filter = "Text files (*.txt)|*.txt";
+            Nullable<bool> result = ofd.ShowDialog();
+
+            if (result.HasValue && result.Value)
+            {
+                string fileName = ofd.FileName;
+
+                if (File.Exists(fileName))
+                {
+                    range = new TextRange();
+                    fStream = new FileStream(fileName, FileMode.Open);
+                    range.Load(fStream, DataFormats.Text);
+                    fStream.Close();
+                }
+            }
+        }
+
+        private bool CanOpen()
+        {
+
+        }
+    }
+}
